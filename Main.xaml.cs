1349638a8b82afc347cb9dcb407a1259f17904c5
oussidi mohamed <youmail@mail.ocm>
2019-12-13 03:15:48 +0100

    First cmmit

---
+++ Main.xaml.cs
@@ -0,0 +1,82 @@
+﻿
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App2
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        private int id_user;
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            FrameNavigationOptions navOptions = new FrameNavigationOptions();
+            navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
+
+            TextBlock ItemContent = args.InvokedItem as TextBlock;
+            if (ItemContent != null)
+            {
+                switch (ItemContent.Tag)
+                {
+                    case "etudiants":
+                        contentFrame.Navigate(typeof(StudentsView));
+                        break;
+
+                    case "filieres":
+                        contentFrame.Navigate(typeof(FilieresView));
+                        break;
+
+                    case "cours":
+                        contentFrame.Navigate(typeof(CoursView));
+                        break;
+
+                    case "absences":
+                        contentFrame.Navigate(typeof(AbsencesView));
+                        break;
+
+                    default:
+                        contentFrame.Navigate(typeof(Main));
+                        break;
+                }
+            }
+        }
+
+        private void MainMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*// set the initial SelectedItem
+            foreach (NavigationViewItemBase item in MainMenu.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "etudiants")
+                {
+                    MainMenu.SelectedItem = item;
+                    break;
+                }
+            }*/
+            contentFrame.Navigate(typeof(StudentsView));
+        }
+
+
+    }
+}
---
+++ views/StudentsView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App2
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class StudentsView : Page
+    {
+        public List<Customer> ListCustomers { get; set; }
+        public StudentsView()
+        {
+            this.InitializeComponent();
+            ListCustomers = Customer.Customers();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder localCache = ApplicationData.Current.LocalCacheFolder;
+            await Launcher.LaunchFolderAsync(localCache);
+        }
+    }
+}
